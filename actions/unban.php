9dd959e330dca59c5a1d638459eca26e214ae71f
Brett Profitt <brett.profitt@gmail.com>
2010-12-05 14:04:58 -0500

    Ban release annotations are deleted when manually unbanning a user. Release annotations work in cron function. Not specifying a length correctly bans forever.

---
+++ actions/unban.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Elgg ban user
+ *
+ * @package Elgg
+ * @subpackage Core
+ */
+
+
+$access_status = access_get_show_hidden_status();
+access_show_hidden_entities(true);
+
+// Get the user
+$guid = get_input('guid');
+$user = get_entity($guid);
+
+if ($user instanceof ElggUser) {
+	if ($user->unban()) {
+		// remove ban_release annotations.
+		$releases = get_annotations($user->guid, '', '', 'ban_release', '', 0, 10, 0, 'desc');
+
+		if ($releases) {
+			foreach ($releases as $release) {
+				$release->delete();
+			}
+		}
+
+		system_message(elgg_echo('admin:user:unban:yes'));
+	} else {
+		register_error(elgg_echo('admin:user:unban:no'));
+	}
+} else {
+	register_error(elgg_echo('admin:user:unban:no'));
+}
+
+access_show_hidden_entities($access_status);
+
+forward($_SERVER['HTTP_REFERER']);
